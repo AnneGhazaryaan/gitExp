557d140984aae2015343747e70f9754f337ebbfd
Anahit Ghazaryan <anahit.ghazaryan@teamsolutions.am>
2024-12-16 19:07:54 +0400

    0
    # Conflicts:
    #	src/main/java/A.java

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,12 +1,10 @@
 public class A {
   public static void g() {
 
-
+    System.out.printf("only this");
     
-    System.out.printf("0");
+    System.out.printf("9");
 
-    System.out.printf("00");
-
-    System.out.println("444");
+    System.out.printf("10");
   }
 }
